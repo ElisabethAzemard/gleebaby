fcf44492737228179f610993f308817982698ba8
Elisabeth Azémard <elie.azemard@gmail.com>
2020-04-08 16:24:05 +0200

    Fill in SubscriptionFactory.

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -6,7 +6,13 @@ use App\Subscription;
 use Faker\Generator as Faker;
 
 $factory->define(Subscription::class, function (Faker $faker) {
+
+    $startingDate = $faker->dateTimeThisYear($max='now');
+    $endingDate   = strtotime('+4 month', $startingDate->getTimestamp());
+
     return [
-        //
+        'start_of_subscription' => $startingDate,
+        'end_of_subscription' => $endingDate,
+        'activation_key' => $faker->randomNumber(20),
     ];
 });
